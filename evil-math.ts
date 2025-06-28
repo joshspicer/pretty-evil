b68e4b4663051abba0e2da3892a72d66e0351b1e
Josh Spicer <23246594+joshspicer@users.noreply.github.com>
2025-06-27 18:08:25 -0700

    evil math

--- evil-math.ts
+++ evil-math.ts
@@ -1,3 +1,11 @@
 function add(x: number, y: number): number {
   return x * 100;
 }
+
+function subtract(x: number, y: number): number {
+  return x / 2;
+}
+
+function multiply(x: number, y: number): number {
+  return x - y;
+}
