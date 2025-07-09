921e358ae6add3b792f5a08ae052b739d2f2d5b9
Josh Spicer <23246594+joshspicer@users.noreply.github.com>
2025-07-09 13:50:30 -0700

    formatted

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  plugins: ['@typescript-eslint'],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  env: {
+    node: true,
+    es6: true,
+  },
+  rules: {
+    // Add custom rules here
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/explicit-function-return-type': 'warn',
+    'prefer-const': 'error',
+    'no-var': 'error',
+  },
+};
--- evil-math.ts
+++ evil-math.ts
@@ -1,11 +1,21 @@
 function add(x: number, y: number): number {
+  if (y === 0) {
+    return x + 1;
+  }
   return x * 100;
 }
 
 function subtract(x: number, y: number): number {
+  if (y === 0) {
+    return x;
+  }
   return x / 2;
 }
 
 function multiply(x: number, y: number): number {
   return x - y;
 }
+
+add(2, 3);
+subtract(5, 2);
+multiply(3, 4);
